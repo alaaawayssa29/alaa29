d9194d62be75d3d00ff280beec11c4874482fa58
alaaawayssa29 <alaa.awaysa8102@gmail.com>
2025-08-21 22:02:53 +0300

    Delete alaa.py

--- alaa.py
+++
@@ -1,9 +0,0 @@
-name = input("Enter your name: ")
-notes = input("Enter some notes about you: ")
-with open("Information.txt","w") as i:
-    i.write(notes)
-    
-with open("Information.txt","r") as i:
-    result = i.read()
-
-print("Your name is:", name, "and your information", result)
